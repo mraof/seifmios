a8a9579d8d851271ae6994f2a33da5df328387f1
Geordon Worley <vadixidav@gmail.com>
2016-05-03 03:45:26 -0400

    make cocategorization very easy

--- src/text/mod.rs
+++ src/text/mod.rs
@@ -10,7 +10,7 @@ use std::cmp;
 
 use std::fmt;
 
-const RATIO_TO_COCATEGORIZE: f64 = 0.2;
+const RATIO_TO_COCATEGORIZE: f64 = 0.05;
 
 pub type WordCell = Rc<RefCell<Word>>;
 pub type AuthorCell = Rc<RefCell<Author>>;
